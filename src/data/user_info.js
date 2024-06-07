fad0d6b79e4e0c64f9f0f940a7b3ebc6e0748f78
Jatinder-003 <jatinderkumarjmmu@gmail.com>
2024-06-07 21:35:13 +0530

    Commit

---
+++ src/data/user_info.js
@@ -0,0 +1,101 @@
+const info = {
+  main: {
+    name: "Jatinder Kr.",
+    description:
+      "I am a software engineer with expertise in full-stack web development. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating projects that follows best practices.",
+    role: "Software Engineer",
+    photo: "../cover.jpg",
+    email: "jatinderkumarjmmu@gmail.com",
+  },
+
+  socials: {
+    github: "https://github.com/Jatinder-003",
+    linkedin: "https://www.linkedin.com/in/jatinder-kumar-161930230",
+    instagram: "https://instagram.com/_jatinder003_",
+  },
+
+  skills: {
+    languages: {
+      description:
+        "I possess proficient expertise in a diverse range of programming languages, including C, C++, C#, Python, TypeScript, JavaScript,and DSA.",
+      image:
+        "https://skillicons.dev/icons?i=c,cpp,cs,py,typescript,javascript,dsa&perline=6&theme=",
+    },
+    frameworks: {
+      description:
+        "Experienced in a versatile tech stack, including React, Express.js, Node.js & Tailwind CSS.",
+      image:
+        "https://skillicons.dev/icons?i=react,express,nodejs,0,0&perline=6&theme=",
+    },
+    tools: {
+      description:
+        "I am well-versed in utilizing various tools and platforms to enhance the development process, including Git, Github, Firebase, and PostgreSQL.",
+      image:
+        "https://skillicons.dev/icons?i=git,github,postgresql,0,0&perline=6&theme=",
+    },
+  },
+
+  projects: [
+    {
+      title: "News App",
+      description:
+        "Stay informed with our comprehensive news app. Access breaking news, trending topics, and in-depth articles from trusted sources worldwide. Customize your feed, follow your favorite topics, and get notified about important updates. Whether you're interested in politics, tech, sports, or entertainment, our app delivers timely updates to keep you informed wherever you go.",
+      technologies:
+        "https://skillicons.dev/icons?i=react,bootstrap,0,0&perline=5&theme=",
+      github: "https://github.com/Jatinder-003/NewsApp",
+    },
+
+    {
+      title: "Tic-Tac-Toe",
+      description:
+        "Tic-Tac-Toe game allows two players to compete on a 3x3 grid. Players take turns marking their symbol (X or O) in an empty cell. The game ends when one player aligns three symbols horizontally, vertically, or diagonally, or when all cells are filled without a winner (resulting in a draw). It includes features for resetting the game and keeping track of the score between multiple rounds.",
+      technologies:
+        "https://skillicons.dev/icons?i=html,javascript,0,0&perline=5&theme=",
+      github: "https://github.com/Jatinder-003/Tic_Tac_Toe",
+      link: "https://thechatroom.netlify.app/",
+    },
+
+    {
+      title: "To-Do List",
+      description:
+        "A dynamic web application crafted with Angular. Engage in an interactive experience where managing tasks becomes a playful challenge. Track your progress and also complete your tasks. Experience seamless integration with Angular's powerful components for a smooth and responsive user interface. Start your journey today and turn your to-do list into an engaging adventure!",
+      technologies:
+        "https://skillicons.dev/icons?i=angular,bootstrap,0,0&perline=5&theme=",
+      github: "https://github.com/Jatinder-003/To-Do_List",
+    },
+
+    {
+      title: "Text_Utils",
+      description:
+        "TextUtils is a word counter and character counting utility which can be used to manipulate your text in the way you want. You can remove extra spaces, copy the manipulated text as well as converted your text from uppercase to lowercase and lowercase to uppercase.",
+      technologies:
+        "https://skillicons.dev/icons?i=react,bootstrap,0,0&perline=5&theme=",
+      github: "https://github.com/Jatinder-003/Text_utils",
+    },
+  ],
+
+  education: [
+    {
+      school: "A. D. Patel Institute Of Technology",
+      degree: "Bachelor in Information Technology",
+      year: "2021 - 2025",
+      image: "images.png",
+    },
+    {
+      school: "Govt. Higher Secondary School, Dablehar, Jammu(J&K)",
+      degree: "Science (Non-Medical)",
+      year: "2019 - 2021",
+      image: "hss.jpg",
+    },
+  ],
+
+  contact: {
+    title: "Let's Get in Touch: Ways to Connect with Me",
+    description:
+      "Thank you for your interest in getting in touch with me. I welcome your feedback, questions, and suggestions. If you have a specific question or comment, please feel free to email me directly at jatinderkumarjmmu@gmail.com. I make an effort to respond to all messages within 24 hours, although it may take me longer during busy periods. Finally, if you prefer to connect on social media, you can connect with me using the social media links below.",
+  },
+
+  footer: "© 2024 Jatinder Kumar. All Rights Reserved",
+};
+
+export default info;
